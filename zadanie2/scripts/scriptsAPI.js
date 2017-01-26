c76dddfe04a0f2d4e5fb027a7b3cf0813e52d5c2
MBorkowski <mb.maciejborkowski@gmail.com>
2017-01-27 00:30:09 +0100

    stylowanie

--- zadanie2/scripts/scriptsAPI.js
+++ zadanie2/scripts/scriptsAPI.js
@@ -1,7 +1,7 @@
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13,
+        center: {lat: 54.372158, lng: 18.638306},
+        zoom: 10,
         mapTypeId: 'roadmap'
     });
 
