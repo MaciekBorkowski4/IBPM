22d8ae4fad7463ffb7e9752b13d19349b5b74c29
MBorkowski <mb.maciejborkowski@gmail.com>
2017-01-26 21:04:33 +0100

    skrypty i html/zmiana organizacji html i funkcjonalności apki, wstępna walidacja pól formularza oraz wyświetlanie zamówienia użytkownika

--- zadanie2/scripts.js
+++ zadanie2/scripts.js
@@ -1,4 +1,9 @@
+$(document).ready(function() {
+
+
+// pobranie danych z formularza i zapisanie do localStorage
 const submitButton = document.querySelector('.inputsForm');
+const ordersList = document.querySelector('.orderList')
 const inputs = [];
 
 function addInputs(e) {
@@ -7,32 +12,59 @@ function addInputs(e) {
     const surname = (this.querySelector('#surname')).value;
     const email = (this.querySelector('#email')).value;
     const password = (this.querySelector('#password')).value;
-    const radio = (((this.querySelector('.radio')).checked === true ) ?
-         ((this.querySelector('#optionsRadios1')).value) : ((this.querySelector('#optionsRadios2')).value));
+    const orderLabel = (this.querySelector('#orderLabel')).value;
+    const orderValue = (this.querySelector('#orderValue')).value;
+
     const textArea = (this.querySelector('#textArea')).value;
-    const pinta = (this.querySelector('#pinta')).value;
-    const kormoran = (this.querySelector('#kormoran')).value;
-    const inneBeczki = (this.querySelector('#inneBeczki')).value;
+
     const input = {
         name: name,
         surname: surname,
         password: password,
         email: email,
-        radio: radio,
-        textArea: textArea,
-        pinta: pinta,
-        kormoran: kormoran,
-        inneBeczki: inneBeczki,
-
+        orderLabel: orderLabel,
+        orderValue: orderValue,
+        textArea: textArea
     };
 
     inputs.push(input);
+    orderList(inputs, ordersList);
     localStorage.setItem('inputs', JSON.stringify(inputs));
     console.log(inputs);
     this.reset();
 }
 
-submitButton.addEventListener('submit', addInputs)
+    function orderList(orders = [], ordersList) {
+        ordersList.innerHTML = orders.map((order, i) => {
+            return ` <li>
+                        <label>Piwo marki: ${order.orderLabel}, liczba sztuk: ${order.orderValue}</label>
+                    </li>
+`;
+        }).join('');
+    }
+
+submitButton.addEventListener('submit', addInputs);
+
+// walidacja formularza
+
+    function nameSurnameValInfo() {
+        const input = $(this);
+        const inputVal = input.val();
+        const nameSurnameLength = input.val().length;
+        if(nameSurnameLength >= 2 && nameSurnameLength <= 19) {
+            input.removeClass('invalid').addClass('valid');
+            input.next('.validationInfo').text('OK!').removeClass('mistake').addClass('ok');
+        } else {
+            input.removeClass('valid').addClass('invalid');
+            input.next('.validationInfo').text('To pole powinno zawierać od 2 do 19 znaków')
+                .removeClass('ok').addClass('mistake');
+        }
+    }
+
+    $('#name').on('blur', nameSurnameValInfo);
+    $('#surname').on('blur', nameSurnameValInfo);
 
 
+});
 //jak przekazywać informacje z radio i checkbox?
+// uzupełnić walidację o string vs number
