043768339c7fe73c57595aaaaf81e1e235fbebb1
MBorkowski <mb.maciejborkowski@gmail.com>
2017-01-26 13:21:15 +0100

    skrypty/ zapisanie do local storage

--- zadanie2/scripts.js
+++ zadanie2/scripts.js
@@ -1,4 +1,5 @@
 const submitButton = document.querySelector('.inputsForm');
+const inputs = [];
 
 function addInputs(e) {
     e.preventDefault();
@@ -6,14 +7,32 @@ function addInputs(e) {
     const surname = (this.querySelector('#surname')).value;
     const email = (this.querySelector('#email')).value;
     const password = (this.querySelector('#password')).value;
-    const inputs = {
+    const radio = (((this.querySelector('.radio')).checked === true ) ?
+         ((this.querySelector('#optionsRadios1')).value) : ((this.querySelector('#optionsRadios2')).value));
+    const textArea = (this.querySelector('#textArea')).value;
+    const pinta = (this.querySelector('#pinta')).value;
+    const kormoran = (this.querySelector('#kormoran')).value;
+    const inneBeczki = (this.querySelector('#inneBeczki')).value;
+    const input = {
         name: name,
         surname: surname,
         password: password,
         email: email,
-        fill: false
-    }
+        radio: radio,
+        textArea: textArea,
+        pinta: pinta,
+        kormoran: kormoran,
+        inneBeczki: inneBeczki,
+
+    };
+
+    inputs.push(input);
+    localStorage.setItem('inputs', JSON.stringify(inputs));
     console.log(inputs);
+    this.reset();
 }
 
 submitButton.addEventListener('submit', addInputs)
+
+
+//jak przekazywać informacje z radio i checkbox?
